23eb24e4cbfcba3a7448817485551a915e53c031
katzman18 <82605935+katzman18@users.noreply.github.com>
2021-04-27 14:17:32 -0400

    Update cachematrix.R
    create function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions allows you to store the inverse of a matrix so
+## it can be called in the future without having to be recalculated
 
-## Write a short comment describing this function
+
+## makeCacheMatrix initializes the function argument as x,
+##  sets the value of m to NULL and defines four other functions
+## the four other functions are stored in a named list so they can easily be retrieved
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve checks to see if there is a stored value of m, the inverse matrix
+## if there is it returns it, if it is NULL it calculates it, stores it, and then prints it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+
+
+T <- matrix(1:4, 2, 2)
+mymatrix <- makeCacheMatrix(T)
+cacheSolve(mymatrix)
